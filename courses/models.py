ccc5ef4c2c734fbb51adfe10b750d7d62efa9f4c
turalmehrali <turalmehrali@gmail.com>
2019-04-03 19:00:53 +0400

    videofield

--- courses/models.py
+++ courses/models.py
@@ -32,6 +32,7 @@ class Course(models.Model):
     title = models.CharField(max_length=100, blank=False)
     teacher = models.ForeignKey(User, default=1, on_delete=models.DO_NOTHING)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    video = models.FileField(upload_to='videos/', null=True, blank=False)
     publish_date = models.DateTimeField(auto_now_add=True)
     purchased = models.PositiveIntegerField(default=0)
     slug = models.SlugField(unique=True, editable=False, max_length=120)
--- webacademia/settings.py
+++ webacademia/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_cleanup',
     'home',
     'courses',
     'accounts',
@@ -66,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -122,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = "/media/"
